a821c0d93d76e3768a72eefeaaee783b5bddb860
AndreaRodas <arodasm6@miumg.edu.gt>
2020-10-21 23:58:17 -0600

    login.html

--- login/index.php
+++ login/index.php
@@ -1,4 +1,5 @@
 <?php
+
   require '../conexion.php';
 
   $message = '';
@@ -6,7 +7,7 @@
   if (!empty($_POST['nombre']) && !empty($_POST['contraseña'])) {
     $sql = "INSERT INTO usuario (nombre, contraseña) VALUES (:nombre, :contraseña)";
     $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':nombre', $_POST['contraseña']);
+    $stmt->bindParam(':nombre', $_POST['nombre']);
     $password = password_hash($_POST['contraseña'], PASSWORD_BCRYPT);
     $stmt->bindParam(':contraseña', $password);
 
@@ -17,7 +18,6 @@
     }
   }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,7 +28,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Iniciar Sesión</title>
+  <title>Taller <3</title>
 
   <!-- Bootstrap core CSS -->
   <link href="taller/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -39,20 +39,19 @@
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
   <!-- Custom styles for this template -->
-  <link href="css/login.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
+<?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+<?php endif; ?>
 
   <div class="overlay"></div>
   <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
     <source src="mp4/video.mp4" type="video/mp4">
   </video>
-  <?php if(!empty($message)): ?>
-      <p> <?= $message ?></p>
-    <?php endif; ?>
-
 
   <div class="masthead">
     <div class="masthead-bg"></div>
@@ -60,20 +59,23 @@
       <div class="row h-100">
         <div class="col-12 my-auto">
           <div class="masthead-content text-white py-5 py-md-0">
-          <form action="iniciar.php" method="POST">
-            <h1 class="mb-3">Taller</h1>
+            <h1 class="mb-3">Iniciar Sesión</h1>
+            <form action="index.php" method="POST">
             <div class="input-group input-group-newsletter">
-      <input name="nombre" type="text" placeholder="Enter your email">
-      <input name="contraseña" type="password" placeholder="Enter your Password">
-      <input name="confirm_password" type="password" placeholder="Confirm Password">
-      <input type="submit" value="Submit">
-    </form>
+              <input type="text" name="nombre" class="form-control" placeholder="Ingresar Usuario" aria-label="Ingresar Usuario">
+              </div>
+             <br></br>
+              <div class="input-group input-group-newsletter">
+                <input type="password" name="contraseña" class="form-control" placeholder="Ingresar Contraseña" aria-label="Ingresar Contraseña"> 
+              </div>
+              <br></br>
+                  <button class="btn btn-secondary" type="submit" id="submit-button"><nav class="sb-sidenav-menu-nested nav"> <a class="nav-link">Confirmar</a></nav></button>
           </div>
         </div>
       </div>
     </div>
   </div> 
-
+</form>
   <!-- Bootstrap core JavaScript -->
   <script src="taller/jquery/jquery.min.js"></script>
   <script src="taller/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -82,4 +84,5 @@
   <script src="js/login.min.js"></script>
 
 </body>
+
 </html>
