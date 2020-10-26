f4c3fb75efdace3b99c2370833ddae7d88fc4577
cindymorhe20 <cmoralesh5@miumg.edu.gt>
2020-10-26 15:11:54 -0600

    vehiculo.php

--- vehiculo.php
+++ vehiculo.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Taller <3</title>
+        <title>Taller-TEK</title>
         <link rel="icon" type="image/x-icon" href="img/carro.ico" />
        
         <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
